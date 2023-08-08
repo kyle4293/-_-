dd1a31deb8829081646717fb20635c9befb05b1c
Youbeen Choi <youbs26@konkuk.ac.kr>
2023-08-08 16:07:19 +0900

    chat add: 나와의 채팅방 상태, message DB 저장 안해서 창 나가면 사라지는 상황.

--- familyAlbum/app/src/main/java/com/example/familyalbum/ChatFragment.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/ChatFragment.kt
@@ -1,17 +1,27 @@
 package com.example.familyalbum
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.view.inputmethod.InputMethodManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.familyalbum.databinding.FragmentChatBinding
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ChatFragment : Fragment() {
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var messageList: ArrayList<Message>
+
+    private lateinit var binding: FragmentChatBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -19,7 +29,64 @@ class ChatFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        binding = FragmentChatBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        messageList = ArrayList()
+        messageAdapter = MessageAdapter(messageList)
+        initLayout()
+    }
+
+
+    private fun initLayout() {
+        binding.chatRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.chatRecyclerView.adapter = messageAdapter
+        sendListening()
+    }
+
+    private fun sendListening() {
+        binding.sendBtn.setOnClickListener {
+            sendMessage()
+        }
+
+        binding.messageEdit.setOnKeyListener { v, keyCode, event ->
+            if ((keyCode == KeyEvent.KEYCODE_ENTER)) {
+                val keyword: String by lazy {
+                    if (binding.messageEdit.text.toString().isNullOrEmpty()) {
+                        return@lazy ""
+                    } else {
+                        return@lazy ""
+                    }
+                }
+                sendMessage()
+            }
+            return@setOnKeyListener false
+        }
+    }
+
+    private fun sendMessage() {
+        val messageText = binding.messageEdit.text.toString().trim()
+        if (messageText.isNotEmpty()) {
+            val currentTime = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date())
+            val message: Message
+            message = Message(messageText, "me", currentTime)
+            messageList.add(message)
+            hideKeyboard()
+        }
+    }
+
+    private fun hideKeyboard() {
+        val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.messageEdit.windowToken, 0)
+        binding.messageEdit.setText("")
+        messageAdapter.notifyItemInserted(messageList.size - 1)
+        binding.chatRecyclerView.scrollToPosition(messageAdapter.itemCount - 1)
+    }
 }
---
+++ familyAlbum/app/src/main/java/com/example/familyalbum/MessageAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.familyalbum
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.familyalbum.databinding.MessageBinding
+
+class MessageAdapter(val messageList: ArrayList<Message>): RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
+    inner class ViewHolder(val binding: MessageBinding): RecyclerView.ViewHolder(binding.root){
+        init{
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageAdapter.ViewHolder {
+        val binding = MessageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MessageAdapter.ViewHolder, position: Int) {
+        val message = messageList[position]
+        holder.binding.receiveMessageText.text = message.message
+        holder.binding.receiveMessageTime.text = message.sendTime
+        holder.binding.receiveMessageSender.text = message.sendId
+    }
+
+    override fun getItemCount(): Int {
+        return messageList.size
+    }
+
+}
