4f8d5d0f30b984fd8462d924217b7730de5ef79c
최창규 <chang4293@gmail.com>
2023-08-08 19:08:35 +0900

    login1

---
+++ familyAlbum/app/src/main/java/com/example/familyalbum/login/LoginActivity.kt
@@ -0,0 +1,10 @@
+package com.example.familyalbum.login
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity {
+
+
+
+}
