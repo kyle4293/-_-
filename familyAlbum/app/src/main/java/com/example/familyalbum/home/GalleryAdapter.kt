2b845768f8e6a41a8e3a320bcddb496fd677516f
Youbeen Choi <youbs26@konkuk.ac.kr>
2023-08-11 00:49:34 +0900

    home에서 카메라 불러오기

--- familyAlbum/app/src/main/java/com/example/familyalbum/home/GalleryAdapter.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/home/GalleryAdapter.kt
@@ -3,6 +3,7 @@ package com.example.familyalbum.home
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.familyalbum.databinding.GallerylistviewBinding
 
 class GalleryAdapter(val galleryList: ArrayList<Gallery>): RecyclerView.Adapter<GalleryAdapter.ViewHolder>() {
@@ -23,7 +24,13 @@ class GalleryAdapter(val galleryList: ArrayList<Gallery>): RecyclerView.Adapter<
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val photo = galleryList[position]
-        holder.binding.photoDate.text = photo.date
-        holder.binding.photoSrc.setImageResource(photo.imgsrc!!.toInt())
+
+        holder.binding.apply {
+            Glide.with(root.context).load(photo.imgsrc)
+                .override(150, 150)
+                .into(photoSrc)
+            photoDate.text = photo.date
+        }
+
     }
 }
--- familyAlbum/app/src/main/java/com/example/familyalbum/home/HomeFragment.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/home/HomeFragment.kt
@@ -1,16 +1,33 @@
 package com.example.familyalbum.home
 
+import android.Manifest
+import android.Manifest.permission_group.STORAGE
 import android.animation.ObjectAnimator
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.familyalbum.MainActivity
 import com.example.familyalbum.R
 import com.example.familyalbum.databinding.FragmentHomeBinding
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class HomeFragment : Fragment() {
@@ -19,6 +36,12 @@ class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var galleryAdapter: GalleryAdapter
     private lateinit var galleryList: ArrayList<Gallery>
+
+    val CAMERA = arrayOf(Manifest.permission.CAMERA)
+    val STORAGE = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    val CAMERA_CODE = 98
+    val STORAGE_CODE = 99
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,6 +85,7 @@ class HomeFragment : Fragment() {
 
         binding.btnCamera.setOnClickListener{
             Toast.makeText(requireContext(), "open camera", Toast.LENGTH_SHORT).show()
+            cameraAction()
         }
 
         binding.btnGallery.setOnClickListener {
@@ -69,6 +93,25 @@ class HomeFragment : Fragment() {
         }
 
     }
+
+    private fun cameraAction() {
+        val itt = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(itt, CAMERA_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //camera로 호출해서 받은거,
+        //gallery로 호출해서 받은거 다르게 처리하기.
+    }
+
+    private fun addImage(uri: Uri) {
+        val currentTime = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date())
+        val picture = Gallery(uri,currentTime )
+
+        galleryList.add(picture)
+    }
+
 
     private fun uploadPhoto() {
         if(isFabOpen){
