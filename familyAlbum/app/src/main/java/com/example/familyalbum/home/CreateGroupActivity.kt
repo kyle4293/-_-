15f6ac19878ddeceb2990d69a14f5f10214e323d
최창규 <chang4293@gmail.com>
2023-08-10 23:36:24 +0900

    groupList

--- familyAlbum/app/src/main/java/com/example/familyalbum/home/CreateGroupActivity.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/home/CreateGroupActivity.kt
@@ -3,6 +3,7 @@ package com.example.familyalbum.home
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.familyalbum.R
 import com.example.familyalbum.databinding.ActivityCreateGroupBinding
@@ -20,5 +21,12 @@ class CreateGroupActivity : AppCompatActivity() {
         // ViewModelProvider를 사용하여 ViewModel 인스턴스 생성
         viewModel = ViewModelProvider(this).get(CreateGroupViewModel::class.java)
         binding.viewModel = viewModel
+
+        // 옵저버 패턴을 사용하여 그룹 생성 성공 시 액티비티 종료
+        viewModel.groupCreationSuccess.observe(this, Observer { isSuccess ->
+            if (isSuccess) {
+                finish() // 그룹 생성 성공 시 액티비티 종료
+            }
+        })
     }
 }
