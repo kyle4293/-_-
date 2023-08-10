f5d6514b6f57af65df62a9fbc0df270f5aef864a
최창규 <chang4293@gmail.com>
2023-08-10 23:03:00 +0900

    group

--- familyAlbum/app/src/main/java/com/example/familyalbum/home/GroupListFragment.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/home/GroupListFragment.kt
@@ -1,25 +1,25 @@
 package com.example.familyalbum.home
-
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.familyalbum.MainActivity
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.familyalbum.databinding.FragmentGroupListBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class GroupListFragment : Fragment() {
 
     private lateinit var binding: FragmentGroupListBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var groupAdapter: GroupAdapter
+    private val firestore = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentGroupListBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -30,15 +30,35 @@ class GroupListFragment : Fragment() {
     }
 
     private fun init() {
-        binding.btnBack.setOnClickListener {
-            //back to home fragement
-            val mActivity = activity as MainActivity
-            mActivity.changeFragment(1)
-        }
+        val dummyGroupList = ArrayList<Group>()
 
-        binding.btnCreateGroup.setOnClickListener {
-            //group create
-        }
+        // 파이어베이스에서 그룹 정보 가져오기
+        firestore.collection("groups")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val groupId = document.id
+                    val groupName = document.getString("groupName") ?: ""
+                    val group = Group(groupId, groupName)
+                    dummyGroupList.add(group)
+                }
 
+                // 가져온 그룹 정보로 어댑터 설정
+                groupAdapter = GroupAdapter(dummyGroupList)
+                binding.groupRecyclerView.apply {
+                    layoutManager = LinearLayoutManager(requireContext())
+                    adapter = groupAdapter
+                }
+            }
+            .addOnFailureListener { exception ->
+                // 오류 처리
+                Toast.makeText(requireContext(), "Failed to fetch group data", Toast.LENGTH_SHORT).show()
+            }
+
+        binding.btnAddGroup.setOnClickListener {
+            // 그룹 추가 화면으로 이동
+            val intent = Intent(requireContext(), CreateGroupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
