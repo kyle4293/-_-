0b09e17edc75c79129b925cf8517a6aadc213c59
최창규 <chang4293@gmail.com>
2023-08-11 23:25:54 +0900

    그룹생성오류 수정

--- familyAlbum/app/src/main/java/com/example/familyalbum/group/GroupInfoDialog.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/group/GroupInfoDialog.kt
@@ -1,12 +1,16 @@
 package com.example.familyalbum.group
 
 import android.app.Dialog
+import android.content.ContentValues
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.familyalbum.databinding.GroupInfoDialogBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class GroupInfoDialog(private val group: Group) : DialogFragment(){
 
@@ -18,6 +22,9 @@ class GroupInfoDialog(private val group: Group) : DialogFragment(){
         binding = GroupInfoDialogBinding.inflate(layoutInflater)
         memberList = ArrayList()
         memberAdapter = GroupMemberAdapter(memberList)
+
+
+        loadAndDisplayGroupUsers(group.groupId) // 그룹원 정보 가져오기
 
         initLayout()
 
@@ -33,6 +40,47 @@ class GroupInfoDialog(private val group: Group) : DialogFragment(){
         binding?.groupId?.text = group.groupId
         binding?.memberRecyclerview?.layoutManager = LinearLayoutManager(requireContext())
         binding?.memberRecyclerview?.adapter = memberAdapter
+    }
+
+    private fun loadAndDisplayGroupUsers(groupId: String) {
+        val firestore = FirebaseFirestore.getInstance()
+        val groupRef = firestore.collection("groups").document(groupId)
+
+        groupRef.get()
+            .addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot.exists()) {
+                    val members = documentSnapshot.get("members") as? ArrayList<String>
+                    members?.let {
+                        fetchAndDisplayUserNames(it)
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.e(ContentValues.TAG, "Error fetching group data: $exception")
+            }
+    }
+
+    private fun fetchAndDisplayUserNames(userIds: List<String>) {
+        val firestore = FirebaseFirestore.getInstance()
+
+        for (userId in userIds) {
+            val userDocRef = firestore.collection("users").document(userId)
+            userDocRef.get()
+                .addOnSuccessListener { documentSnapshot ->
+                    if (documentSnapshot.exists()) {
+                        val userName = documentSnapshot.getString("name")
+                        val userImage = documentSnapshot.getString("image") ?: ""
+                        if (!userName.isNullOrEmpty()) {
+                            val memberInfo = MemberInfo(userName, userImage)
+                            memberList.add(memberInfo) // 리스트에 MemberInfo 객체 추가
+                            memberAdapter.notifyDataSetChanged() // 어댑터 갱신
+                        }
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    // 실패 시 처리 로직
+                }
+        }
     }
 
     override fun onAttach(context: Context) {
