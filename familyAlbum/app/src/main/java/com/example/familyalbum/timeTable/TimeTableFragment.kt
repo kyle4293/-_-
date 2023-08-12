7640530c2c8c0f96bb1548da3ecc5946f1c7f694
haeun1 <haeun1120@konkuk.ac.kr>
2023-08-13 03:18:34 +0900

    a

--- familyAlbum/app/src/main/java/com/example/familyalbum/timeTable/TimeTableFragment.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/timeTable/TimeTableFragment.kt
@@ -113,7 +113,7 @@ class TimeTableFragment : Fragment() {
             }
         }
 
-        setupProfile()
+        myProfile()
         binding.imageView.setOnClickListener {
             showDialog()
         }
@@ -125,7 +125,7 @@ class TimeTableFragment : Fragment() {
 
     }
 
-    private fun setupProfile() {
+    private fun myProfile() {
         firebaseAuth = FirebaseAuth.getInstance()
         firestore = FirebaseFirestore.getInstance()
         var storage = FirebaseStorage.getInstance()
@@ -162,9 +162,9 @@ class TimeTableFragment : Fragment() {
     }
     fun showDialog() {
         val alertDialogBuilder = AlertDialog.Builder(requireContext())
-        alertDialogBuilder.setTitle("이미지 클릭 다이얼로그")
+        alertDialogBuilder.setTitle("우리가족 시간표 선택")
         alertDialogBuilder.setMessage("이미지를 클릭하셨습니다.")
-        alertDialogBuilder.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+        alertDialogBuilder.setPositiveButton("닫기", DialogInterface.OnClickListener { dialog, which ->
             // 확인 버튼 클릭 시 실행할 작업
             dialog.dismiss() // 다이얼로그 닫기
         })
@@ -301,10 +301,14 @@ class TimeTableFragment : Fragment() {
                     val alertDialogBuilder = AlertDialog.Builder(requireContext())
                     alertDialogBuilder.setTitle("스케줄 상세정보")
                     alertDialogBuilder.setMessage("${start.text},${end.text},${name.text},${place.text}")
-                    alertDialogBuilder.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
-                        // 확인 버튼 클릭 시 실행할 작업
+                    alertDialogBuilder.setPositiveButton("삭제", DialogInterface.OnClickListener { dialog, which ->
+                        parentView.removeView(customLayout)
                         dialog.dismiss() // 다이얼로그 닫기
                     })
+                    .setNegativeButton("수정") { dialog, _ ->
+
+                        dialog.dismiss() // 다이얼로그 닫기
+                    }
                     alertDialogBuilder.show()
                 }
                 parentView.addView(customLayout)  // 인플레이션 된 사용자 정의 레이아웃을 부모 뷰에 추가
