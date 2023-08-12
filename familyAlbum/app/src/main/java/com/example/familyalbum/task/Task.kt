f93d5c78ad444ee4c6d87e58e2bbf59ff7eda62f
bzzun0609 <punpon53@naver.com>
2023-08-13 02:24:01 +0900

    ..

--- familyAlbum/app/src/main/java/com/example/familyalbum/task/Task.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/task/Task.kt
@@ -1,10 +1,10 @@
 package com.example.familyalbum.task
 
 data class Task(
-    val userName: String = "",            //User Id
-    val title: String = "",             //Task
-    val place: String = "",             //장소
     val dayOfWeek: String = "",         //요일(월,화...)
+    val endTime: String = "",            //Task 종료시간
+    val place: String = "",             //장소
     val startTime: String = "",         //Task 시작시간
-    val endTime: String = ""            //Task 종료시간
+    val title: String = "",             //Task
+    val userName: String = ""            //User Id
 )
