2eded44ed159160ce9a6979892695a8158154cb3
haeun1 <haeun1120@konkuk.ac.kr>
2023-08-13 23:47:44 +0900

    a

--- familyAlbum/app/src/main/java/com/example/familyalbum/tip/TipEditActivity.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/tip/TipEditActivity.kt
@@ -1,7 +1,11 @@
 package com.example.familyalbum
 
+import android.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.widget.ArrayAdapter
 import com.example.familyalbum.databinding.ActivityTipEditBinding
 
 class TipEditActivity : AppCompatActivity() {
@@ -11,14 +15,49 @@ class TipEditActivity : AppCompatActivity() {
         binding = ActivityTipEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //요 밑 세가지가 수정 전 tip 정보
         val title = intent.getStringExtra("title")
-        val content = intent.getStringExtra("content")
+        val contents = intent.getStringExtra("contents")
         val tag = intent.getStringExtra("tag")
-        val user = intent.getStringExtra("user")
 
-        binding.tiptitle.text = title
-        binding.content.text = content
-        binding.edittag.text = tag
-        binding.user.text = user
+        binding.inputTipTitle.text =  Editable.Factory.getInstance().newEditable(title)
+
+        val contentStringBuilder = StringBuilder()
+        contents?.forEach { content ->
+            contentStringBuilder.append(content)
+            contentStringBuilder.append("\n")
+        }
+
+        binding.inputTipContent.text = Editable.Factory.getInstance().newEditable(contentStringBuilder.toString())
+
+        val tagspinner = binding.tagSpinner
+
+        val tags = listOf("의","식","주")
+
+        val tagAdapter = ArrayAdapter(this, R.layout.simple_spinner_item, tags)
+        tagAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        tagspinner.adapter = tagAdapter
+
+        if(tag != null) {
+            val defaultSelection = tag
+            val position = tags.indexOf(defaultSelection)
+            if (position != -1) {
+                tagspinner.setSelection(position)
+            }
+        }
+
+        binding.button2.setOnClickListener {
+
+            //여기서 DB작업을 해주면 됩니다
+
+            //새로운 tip 정보
+            val newTipTitle = binding.inputTipTitle.text.toString()
+            val newTipContent = binding.inputTipContent.text.toString()
+            val newTipTag = binding.tagSpinner.selectedItem.toString()
+
+            //원래의 tip정보로 db를 찾은다음 그 db를 위의 새로운 tip정보로 수정
+
+
+        }
     }
 }
