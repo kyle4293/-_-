0c93d98c1903488d6d66ab5fcff21db8c2e8955e
최창규 <chang4293@gmail.com>
2023-08-13 20:26:49 +0900

    채팅기능

--- familyAlbum/app/src/main/java/com/example/familyalbum/chat/ChatFragment.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/chat/ChatFragment.kt
@@ -1,15 +1,16 @@
 package com.example.familyalbum.chat
 
-import android.content.Context
 import android.os.Bundle
-import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.familyalbum.MainActivity
 import com.example.familyalbum.databinding.FragmentChatBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
@@ -18,79 +19,105 @@ class ChatFragment : Fragment() {
     private lateinit var messageAdapter: MessageAdapter
     private lateinit var messageList: ArrayList<Message>
     private lateinit var binding: FragmentChatBinding
+    private lateinit var currentUserID: String
+    private lateinit var chatRoomId: String
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    companion object {
+        private const val ARG_GROUP_ID = "group_id"
+
+        fun newInstance(groupId: String): ChatFragment {
+            val fragment = ChatFragment()
+            val args = Bundle()
+            args.putString(ARG_GROUP_ID, groupId)
+            fragment.arguments = args
+            return fragment
+        }
     }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentChatBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val groupId = (activity as MainActivity).selectedGroupId
+        chatRoomId = "group_$groupId"
+
+        currentUserID = FirebaseAuth.getInstance().currentUser?.uid ?: ""
+
+
         init()
     }
 
     private fun init() {
         messageList = ArrayList()
         messageAdapter = MessageAdapter(messageList)
-        initLayout()
-    }
-
-    private fun initLayout() {
         binding.chatRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.chatRecyclerView.adapter = messageAdapter
-        sendListening()
-    }
 
-    private fun sendListening() {
+        // 채팅 메시지 불러오기 및 실시간 감지
+        loadAndListenForMessages(chatRoomId)
+
         binding.sendBtn.setOnClickListener {
             sendMessage()
         }
-
-        //enter key event
-        binding.messageEdit.setOnKeyListener { v, keyCode, event ->
-            if ((keyCode == KeyEvent.KEYCODE_ENTER)) {
-                val keyword: String by lazy {
-                    if (binding.messageEdit.text.toString().isNullOrEmpty()) {
-                        return@lazy ""
-                    } else {
-                        return@lazy ""
-                    }
-                }
-                sendMessage()
-            }
-            return@setOnKeyListener false
-        }
     }
+
+    private fun loadAndListenForMessages(chatRoomId: String) {
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("chatRooms")
+            .document(chatRoomId)
+            .collection("messages")
+            .orderBy("timestamp")
+            .addSnapshotListener { querySnapshot, _ ->
+                val messages = querySnapshot?.documents?.mapNotNull { document ->
+                    val senderId = document.getString("senderId")
+                    val message = document.getString("message")
+                    val timestamp = document.getTimestamp("timestamp")?.toDate()
+                    if (senderId != null && message != null && timestamp != null) {
+                        Message(message, senderId, timestamp)
+                    } else {
+                        null
+                    }
+                } ?: emptyList()
+
+                messageList.clear()
+                messageList.addAll(messages)
+                messageAdapter.notifyDataSetChanged()
+
+                // Scroll to the bottom
+                binding.chatRecyclerView.scrollToPosition(messageAdapter.itemCount - 1)
+            }
+    }
+
 
     private fun sendMessage() {
         val messageText = binding.messageEdit.text.toString().trim()
         if (messageText.isNotEmpty()) {
             val currentTime = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date())
 
-            //message list에 추가.
-            val message = Message(messageText, "me", currentTime)
-            messageList.add(message)
-            hideKeyboard()
+            val db = FirebaseFirestore.getInstance()
+            val messageData = hashMapOf(
+                "senderId" to currentUserID,
+                "message" to messageText,
+                "timestamp" to FieldValue.serverTimestamp()
+            )
+
+            db.collection("chatRooms")
+                .document(chatRoomId)
+                .collection("messages")
+                .add(messageData)
+                .addOnSuccessListener {
+                    binding.messageEdit.text.clear()
+                }
         }
-    }
-
-    private fun hideKeyboard() {
-        //keyboard 내리기
-        val imm = requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(binding.messageEdit.windowToken, 0)
-
-        //message line 비우기
-        binding.messageEdit.setText("")
-        //adapter에 notify, 가장 최근 채팅 내역으로 자동 스크롤.
-        messageAdapter.notifyItemInserted(messageList.size - 1)
-        binding.chatRecyclerView.scrollToPosition(messageAdapter.itemCount - 1)
     }
 }
