96cd84e7520a83bdf3667c068ead32a72ee4fd04
최창규 <chang4293@gmail.com>
2023-08-14 16:53:02 +0900

    일단 채팅 순서만

--- familyAlbum/app/src/main/java/com/example/familyalbum/chat/ChatItem.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/chat/ChatItem.kt
@@ -2,22 +2,20 @@ package com.example.familyalbum.chat
 
 import java.util.*
 
-interface ChatItem {
-
+sealed class ChatItem {
     data class MyMessage(
-        var message: String?,
-        var senderId: String?,
-        var timestamp: Date?
-    ): ChatItem
+        val message: String?,
+        val senderId: String?,
+        val timestamp: Date?
+    ) : ChatItem()
 
     data class OtherMessage(
-        var message: String?,
-        var senderId: String?,
-        var senderName: String,
-        var timestamp: Date?,
-        var senderImg: String
-    ): ChatItem
+        val message: String?,
+        val senderId: String?,
+        val senderName: String,
+        val timestamp: Date?,
+        val senderImg: String
+    ) : ChatItem()
 
-    data class DateSeparator(var date: Date) : ChatItem
-
+    data class DateSeparator(val date: Date) : ChatItem()
 }
