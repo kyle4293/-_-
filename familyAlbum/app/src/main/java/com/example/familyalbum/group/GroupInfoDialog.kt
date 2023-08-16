9af2d94465ef1584806a66028261058c241109da
최창규 <chang4293@gmail.com>
2023-08-16 22:26:10 +0900

    그룹 삭제 기능

--- familyAlbum/app/src/main/java/com/example/familyalbum/group/GroupInfoDialog.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/group/GroupInfoDialog.kt
@@ -3,12 +3,18 @@ package com.example.familyalbum.group
 import android.app.Dialog
 import android.content.ContentValues
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.familyalbum.MainActivity
+import com.example.familyalbum.R
 import com.example.familyalbum.databinding.GroupInfoDialogBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 
 
@@ -41,7 +47,63 @@ class GroupInfoDialog(private val group: Group) : DialogFragment(){
         binding?.groupId?.text = group.groupId
         binding?.memberRecyclerview?.layoutManager = LinearLayoutManager(requireContext())
         binding?.memberRecyclerview?.adapter = memberAdapter
+
+        binding?.btnDeleteGroup?.setOnClickListener {
+            deleteGroup(group.groupId, group.groupName)
+        }
     }
+
+    private fun deleteGroup(groupId: String?, groupName: String?) {
+        if (groupId == null || groupName == null) {
+            return
+        }
+        val firestore = FirebaseFirestore.getInstance()
+        val groupRef = firestore.collection("groups").document(groupId)
+
+        groupRef.delete()
+            .addOnSuccessListener {
+                // 그룹 문서 삭제 성공
+                deleteGroupInfoFromUserGroups(groupId, groupName)
+            }
+            .addOnFailureListener { exception ->
+                // 그룹 문서 삭제 실패
+                Toast.makeText(requireContext(), "Failed to delete group $groupName", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun deleteGroupInfoFromUserGroups(groupId: String, groupName: String) {
+        val firestore = FirebaseFirestore.getInstance()
+
+        // 사용자 컬렉션에서 groupId와 groupName을 삭제하는 로직
+        firestore.collection("users").get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val userRef = firestore.collection("users").document(document.id)
+
+                    userRef.get()
+                        .addOnSuccessListener { userDoc ->
+                            val userGroups = userDoc.get("groups") as? List<Map<String, String>>
+                            val updatedGroups = userGroups?.filter { groupMap ->
+                                val groupGroupId = groupMap["groupId"]
+                                val groupGroupName = groupMap["groupName"]
+                                groupGroupId != groupId || groupGroupName != groupName
+                            }
+
+                            if (updatedGroups != userGroups) {
+                                userRef.update("groups", updatedGroups)
+                            }
+
+                            val intent = Intent(requireContext(), MainActivity::class.java)
+                            startActivity(intent)
+                        }
+                }
+            }
+            .addOnFailureListener { exception ->
+                // 실패 시 처리 로직
+            }
+    }
+
+
 
     private fun loadAndDisplayGroupUsers(groupId: String) {
         val firestore = FirebaseFirestore.getInstance()
