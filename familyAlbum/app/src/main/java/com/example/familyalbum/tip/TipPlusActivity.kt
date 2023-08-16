ef0858a64607cd29e79087574969207a9349f9bc
haeun1 <87661039+haeun1@users.noreply.github.com>
2023-08-16 22:45:25 +0900

    tip

--- familyAlbum/app/src/main/java/com/example/familyalbum/tip/TipPlusActivity.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/tip/TipPlusActivity.kt
@@ -54,6 +54,7 @@ class TipPlusActivity : AppCompatActivity() {
                     // 추가 성공 시 처리
                     //finish() // 예를 들어, 현재 화면을 종료하거나 다른 처리 가능
                     val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("fromTipEdit","fromTipEdit")
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
                     intent.putExtra("groupId", currentGroupId) // 그룹 정보 전달
                     intent.putExtra("groupName", currentGroupName) // 그룹 이름 전달
