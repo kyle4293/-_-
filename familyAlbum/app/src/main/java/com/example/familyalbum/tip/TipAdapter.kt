a3f0aca18c5350da0e6d014645aef1c7da478669
haeun1 <87661039+haeun1@users.noreply.github.com>
2023-08-16 22:01:35 +0900

    tip,table

--- familyAlbum/app/src/main/java/com/example/familyalbum/tip/TipAdapter.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/tip/TipAdapter.kt
@@ -1,33 +1,27 @@
 package com.example.familyalbum.tip
 
+import android.content.ContentValues
 import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.familyalbum.MainActivity
 import com.example.familyalbum.R
 import com.example.familyalbum.TipEditActivity
 import com.example.familyalbum.databinding.TipBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class TipAdapter(private val currentGroupId: String, private val currentGroupName: String, private var tipList: List<Tip>): RecyclerView.Adapter<TipAdapter.ViewHolder>() {
     inner class ViewHolder( val binding: TipBinding): RecyclerView.ViewHolder(binding.root){
         init{
-            binding.editbutton.setOnClickListener {
-                val context = it.context
-                val position = adapterPosition
-                if (position != RecyclerView.NO_POSITION) {
-                    val tip = tipList[position]
-                    val intent = Intent(context, TipEditActivity::class.java)
 
-                    intent.putExtra("groupId", currentGroupId)
-                    intent.putExtra("groupName", currentGroupName)
-                    intent.putExtra("title", tip.title)
-                    intent.putExtra("content", tip.content) // 이 역시 필요한 타입에 맞게 수정
-                    intent.putExtra("tag", tip.tag)
-                    context.startActivity(intent)
-                }
-            }
         }
     }
 
@@ -38,7 +32,9 @@ class TipAdapter(private val currentGroupId: String, private val currentGroupNam
 
     override fun onBindViewHolder(holder: TipAdapter.ViewHolder, position: Int) {
         val tip = tipList[position]
-
+        holder.binding.editbutton.setOnClickListener {
+            showPopupMenu(it,position)
+        }
         holder.binding.tipTitle.text = tip.title
         holder.binding.tag.text = tip.tag
         when (tip.tag) {
@@ -51,6 +47,85 @@ class TipAdapter(private val currentGroupId: String, private val currentGroupNam
         Log.d("TipAdapter", "Contents: ${tip.content}")
     }
 
+    private fun showPopupMenu(view: View,position: Int) {
+        var firestore = FirebaseFirestore.getInstance()
+
+
+        val popup = PopupMenu(view.context, view)
+        val inflater: MenuInflater = popup.menuInflater
+        inflater.inflate(R.menu.tip_menu, popup.menu)
+        popup.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.action_edit -> {
+                    // Handle edit action
+                    val context = view.context
+                    if (position != RecyclerView.NO_POSITION) {
+                        val tip = tipList[position]
+                        val intent = Intent(context, TipEditActivity::class.java)
+
+                        intent.putExtra("groupId", currentGroupId)
+                        intent.putExtra("groupName", currentGroupName)
+                        intent.putExtra("title", tip.title)
+                        intent.putExtra("content", tip.content) // 이 역시 필요한 타입에 맞게 수정
+                        intent.putExtra("tag", tip.tag)
+                        context.startActivity(intent)
+                    }
+                    true
+                }
+                R.id.action_delete -> {
+                    // Handle delete action
+                    val tip = tipList[position]
+                    val query = firestore.collection("tips")
+                        .whereEqualTo("title", tip.title)
+                        .whereEqualTo("content", tip.content)
+                        .whereEqualTo("tag", tip.tag)
+
+                    lateinit var tipId: String
+                    query.addSnapshotListener { querySnapshot, _ ->
+                        for (document in querySnapshot!!.documents) {
+                            tipId = document.id
+                        }
+                    }
+
+                    val builder = AlertDialog.Builder(view.context)
+                    builder.setTitle("잔소리 삭제")
+                    builder.setMessage("삭제버튼 누르면 모든 가족들에게도 삭제됩니다. 정말로 삭제하시겠습니까?")
+                    builder.setPositiveButton("삭제") { dialog, which ->
+                        if (tipId != null) {
+                            // tipId가 초기화된 경우에만 삭제 로직 실행
+                            val tipDocRef = firestore.collection("tips").document(tipId)
+                            tipDocRef.delete()
+                                .addOnSuccessListener {
+                                    val intent = Intent(view.context, MainActivity::class.java)
+                                    intent.putExtra("fromTipEdit", "fromTipEdit")
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                                    intent.putExtra("groupId", currentGroupId) // 그룹 정보 전달
+                                    intent.putExtra("groupName", currentGroupName) // 그룹 이름 전달
+                                    view.context.startActivity(intent)
+                                }
+                                .addOnFailureListener { e ->
+                                    // 삭제 실패 시 처리
+                                    Log.e(ContentValues.TAG, "Error deleting document", e)
+                                }
+                        } else {
+                            Log.e(ContentValues.TAG, "tipId is not initialized")
+                        }
+                        // 원래의 tip정보로 db를 찾은다음, 그 db삭제
+
+                    }
+                    builder.setNegativeButton("취소") { dialog, which ->
+                        // "취소" 버튼 클릭 시 처리
+                    }
+                    val dialog = builder.create()
+                    dialog.show()
+
+                    true
+                }
+                else -> false
+            }
+        }
+        popup.show()
+    }
 
     override fun getItemCount(): Int {
         return tipList.size
