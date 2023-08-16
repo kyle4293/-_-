73027f0204dd374b56f562195f320b3b4c9b9d48
최창규 <chang4293@gmail.com>
2023-08-16 23:46:13 +0900

    그룹 생성시 바로 적용

--- familyAlbum/app/src/main/java/com/example/familyalbum/group/CreateGroupActivity.kt
+++ familyAlbum/app/src/main/java/com/example/familyalbum/group/CreateGroupActivity.kt
@@ -1,9 +1,11 @@
 package com.example.familyalbum.group
 
 import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +17,7 @@ import com.example.familyalbum.MainActivity
 import com.example.familyalbum.R
 import com.example.familyalbum.databinding.ActivityCreateGroupBinding
 import com.example.familyalbum.profile.ProfileConfirmActivity
+import com.google.firebase.firestore.FirebaseFirestore
 import java.io.IOException
 
 class CreateGroupActivity : AppCompatActivity() {
@@ -23,6 +26,7 @@ class CreateGroupActivity : AppCompatActivity() {
     private lateinit var viewModel: CreateGroupViewModel
     private var imageUri: Uri? = null
     var REQUEST_CONFIRM = 0
+
 
     private val imagePickerLauncher =
         registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
@@ -52,8 +56,7 @@ class CreateGroupActivity : AppCompatActivity() {
         viewModel.groupCreationSuccess.observe(this, Observer { isSuccess ->
             if (isSuccess) {
                 Toast.makeText(this, "Succeeded in creating group", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+                joinAndLoadMain(viewModel.groupJoinId.value.toString())
             } else {
                 // 그룹 생성 실패 시 처리
                 Toast.makeText(this, "Failed to create group", Toast.LENGTH_SHORT).show()
@@ -87,5 +90,29 @@ class CreateGroupActivity : AppCompatActivity() {
                 .load(imageUri)
                 .into(binding.groupImg)
         }
+    }
+
+    private fun joinAndLoadMain(groupId: String) {
+        val firestore = FirebaseFirestore.getInstance()
+        val groupRef = firestore.collection("groups").document(groupId)
+        groupRef.get()
+            .addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot.exists()) {
+                    val groupName = documentSnapshot.getString("groupName")
+                    if (groupName != null) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                        intent.putExtra("groupId", groupId) // 그룹 정보 전달
+                        intent.putExtra("groupName", groupName) // 그룹 이름 전달
+                        startActivity(intent)
+                        finish()
+                    } else {
+                    }
+                } else {
+                }
+            }
+            .addOnFailureListener { exception ->
+                // 오류 처리
+            }
     }
 }
